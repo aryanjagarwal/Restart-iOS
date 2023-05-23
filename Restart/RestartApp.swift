59279ba7fce41cd1bbb47b9e1068b12fb0984f11
Aryan Jagarwal <ajbaggar@gmail.com>
2023-05-23 09:51:51 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Aryan Jagarwal on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
